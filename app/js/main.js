9692c4a5bdf15503301638e7e4679166a90f73e1
David Olivari <david.olivari@gmail.com>
2016-04-11 16:27:38 +0200

    Login working for FB and Twitter

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,11 @@
 /*global $:true Popcorn:true*/
 
 $(function() {
+  var pos;
 
   function showPosition(position) {
+    pos = position;
     var latlon = position.coords.latitude + "," + position.coords.longitude;
-
     var img_url = 'http://maps.googleapis.com/maps/api/staticmap?center=' + latlon + '&zoom=15&size=400x300&sensor=false';
 
     document.getElementById('myLoc').innerHTML = '<img src="' + img_url + '">';
@@ -18,6 +19,8 @@ $(function() {
 
 
   var a = document.getElementById('myAudio');
+  //a.play();
+
   var c = 1;
   var i = setInterval(function() {
     var t = a.currentTime;
@@ -36,15 +39,64 @@ $(function() {
 
 
 
-  var ref = new Firebase("https://tem.firebaseio.com");
+  var ref = new Firebase("https://tem.firebaseio.com/users");
+
+  function authenticate(authData) {
+    var myRef = new Firebase("https://tem.firebaseio.com/users/" + authData.uid + "/log");
+
+    myRef.orderByChild("timestamp").limitToLast(5).on('child_added', function(snap) {
+      var val = snap.val();
+      $('#log').append('<li>' + new Date(val.timestamp) + ' : ' + val.event + '</li>');
+    });
+
+    var log = myRef.push({
+      event: 'login',
+      commissure: 1,
+      timestamp: Date.now(),
+      latitude: pos.coords.latitude,
+      longitude: pos.coords.longitude
+    });
+
+    $(window).bind('beforeunload', function() {
+      var log = myRef.push({
+        event: 'leave',
+        commissure: 1,
+        timestamp: Date.now(),
+        latitude: pos.coords.latitude,
+        longitude: pos.coords.longitude,
+        position: a.currentTime
+      });
+    });
+
+
+  }
+
+  var myRef;
   $.fbLog = function() {
     ref.authWithOAuthPopup("facebook", function(error, authData) {
       if (error) {
         console.log("Login Failed!", error);
       } else {
-        console.log("Authenticated successfully with payload:", authData);
+        console.log("Authenticated successfully with payload:", authData.facebook);
+        $('#dispname').text(authData.facebook.displayName + ' / ' + authData.uid)
+        authenticate(authData);
       }
     });
   }
+
+  $.twLog = function() {
+    ref.authWithOAuthPopup("twitter", function(error, authData) {
+      if (error) {
+        console.log("Login Failed!", error);
+      } else {
+        console.log("Authenticated successfully with payload:", authData);
+        $('#dispname').text(authData.twitter.displayName + ' / ' + authData.uid)
+        authenticate(authData);
+      }
+    });
+  }
+
+
+
 
 });
